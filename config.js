f6fff50871c5ca7dbe67b49d2ee5e6f58d980099
Adam Basiak <basiakus@gmail.com>
2017-12-29 18:06:52 +0100

    start

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '169304895421-gtodav3se1vuphgac6spod04k1ke24vq.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRETS': '0xgzK6mHTt629oXKyzrHr_U7',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
